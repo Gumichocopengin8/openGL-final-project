576200dce588fe59313a150c39fa8ca908fe4634
Keita Nonaka <iKonnyaku40@yahoo.co.jp>
2019-05-03 12:50:07 -0400

    clound not finished yet

---
+++ structures/Cloud.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Keita Nonaka on 2019-05-03.
+//
+
+#include "Cloud.h"
+
+Cloud::Cloud() {
+
+}
---
+++ structures/Cloud.h
@@ -0,0 +1,15 @@
+//
+// Created by Keita Nonaka on 2019-05-03.
+//
+
+#ifndef PROJECT_CLOUD_H
+#define PROJECT_CLOUD_H
+
+
+class Cloud {
+public:
+    Cloud();
+};
+
+
+#endif //PROJECT_CLOUD_H
